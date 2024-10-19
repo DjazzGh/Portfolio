67d55125984d429b207d17a4ff57ac47e1f223cc
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-10-19 07:10:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("animate");
+                } else {
+                    entry.target.classList.remove("animate");
+                }
+            });
+        },
+        { threshold: 0.1 }
+    );
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
+});
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    const buttons = document.querySelectorAll('.nav-links button');
+    
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    const activeButton = document.querySelector(`[onclick="scrollToSection('${sectionId}')"]`);
+    activeButton.classList.add('active');
+
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
